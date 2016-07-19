af7c594683ea443c337cfad85b1446b51bffad70
s-nakajima <nakajimashouhei@gmail.com>
2016-07-19 15:50:55 +0900

    ルーティングの見直し https://github.com/NetCommons3/NetCommons3/issues/309 https://github.com/NetCommons3/NetCommons/pull/284

---
+++ Config/routes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * routes configuration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+Router::connect(
+	'/user_roles/:controller/:action/*',
+	array('plugin' => 'user_roles')
+);
